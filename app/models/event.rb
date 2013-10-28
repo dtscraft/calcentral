75a6861aed7eb129afadc0bc2caeb37ab2766468
David Su <dtscraft@gmail.com>
2013-10-27 20:17:32 -0700

    Event.find_by_category

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
   #http://guides.rubyonrails.org/active_record_querying.html#passing-in-arguments
+  scope :find_by_category, ->(category){ includes(:club => :categories).where("categories.name = ?", category)}
   scope :find_by_club, ->(club){ includes(:club).where("clubs.name = ? ", club)}
   scope :chronological_order, order("start_time ASC")
   scope :reverse_chronological_order, order("start_time DESC")

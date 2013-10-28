5c1ca6f98c12007d5446794574fd989f352679f4
David Su <dtscraft@gmail.com>
2013-10-27 20:08:40 -0700

    added Event.find_by_club
    find by club accepts a club name as an argument
    also added class methods to order by start_time:
        Event.chronological_order
        Event.reverse_chronological_order

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
+  #http://guides.rubyonrails.org/active_record_querying.html#passing-in-arguments
+  scope :find_by_club, ->(club){ includes(:club).where("clubs.name = ? ", club)}
+  scope :chronological_order, order("start_time ASC")
+  scope :reverse_chronological_order, order("start_time DESC")
   attr_accessible :description, :end_time, :name, :start_time
   belongs_to :club
   has_many :categories, :through => :club, :source => :categories	

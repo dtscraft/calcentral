ed1837def57ccf76642394a38ca1e1d56aa24bb4
David Su <dtscraft@gmail.com>
2013-11-09 19:06:46 -0800

    Cucumber test for Event.get_facebook_group_events
    TODO: write a sad path for when a facebook group does not have an id
    Maybe change get_facebook_group_events to an instance method of Club

--- features/group_events.feature
+++ features/group_events.feature
@@ -1,13 +1,10 @@
 Feature: Retrieve events from Facebook Groups.
 
-         As a person signing into Facebook in calcentral
-         So I get event id(s) from Facebook Groups id(s)
-         I want to be able store each event id(s) in calcentral database
+         As a compute programmer who wants to automate the process of retrieving Facebook Group Events
+         So that I get facebook event data from a facebook group
+         I want query the facebook api and store the events into the database
 
-Background:
-   Given I have user access token
-
-Scenario: Get events for Club_1
-    Given I want facebook events for club 1 #nothing
-    When I run a background #nothing
-    Then I should get all facebook events for club 1 #run a test
+Scenario: Get events for a club that  has an associated Facebook Group
+    Given I want facebook events for facebook group "Hackers at Berkeley"
+    When I get facebook events for Facebook Group "276905079008757" with user access token "fbaccesstoken"
+    Then there should be 110 events for facebook club 276905079008757
--- features/step_definitions/group_events.rb
+++ features/step_definitions/group_events.rb
@@ -1,19 +1,17 @@
-Given /I have user access token/  do
-	  body = 'you'
-      FakeWeb.register_uri(:get, "http://google.com/", :body => body)
-      response = Net::HTTP.get(URI.parse("http://google.com/"))
-      assert response == body
+Given /I want facebook events for facebook group "([^"]*)"/ do |club_id| end
+
+When /I get facebook events for Facebook Group "([^"]*)" with user access token "([^"]*)"/  do | graph_id, user_access_token|
+      url = "https://graph.facebook.com/#{graph_id}/events?access_token=#{user_access_token}"
+      body = File.open( File.join(File.expand_path(File.dirname(__FILE__)), "..", "support", "events.json"), "r").read
+      FakeWeb.register_uri(:get, url, :body => body)
+      events = Event.get_facebook_group_events(graph_id, user_access_token)
+      events.data.each{ |event|
+          club = (Club.find_by_facebook_id(graph_id) or Club.create!(facebook_id: graph_id))
+          new_event = Event.create!(name: event.name, start_time: event.start_time, end_time: event.end_time, facebook_id: event.id)
+          club.events << new_event and club.save!
+      }
 end
 
-
-Given /I want facebook events for club 1/  do
-	
-end
-
-When /I run a background/  do
-	
-end
-
-Then /I should get all facebook events for club 1/  do
-	
+Then /there should be (.*) events for facebook club (.*)/ do |n, facebook_id|
+    assert Club.find_by_facebook_id!(facebook_id).events.count == n.to_i, "got #{Club.find_by_facebook_id!(facebook_id).events.count} events instead of #{n} events for club with facebook id #{facebook_id}"
 end

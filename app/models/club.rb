528e6727c39f115630b05dfbe8b8ae2a7ed22f39
David Su <dtscraft@gmail.com>
2013-11-09 18:58:35 -0800

    can set facebook id on create for Club and Event

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,5 +1,5 @@
 class Club < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :facebook_id
   has_many :events 
   has_many :categories
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,10 @@ class Event < ActiveRecord::Base
   scope :find_by_club, ->(club){ includes(:club).where("clubs.name = ? ", club)}
   scope :chronological_order, order("start_time ASC")
   scope :reverse_chronological_order, order("start_time DESC")
-  attr_accessible :description, :end_time, :name, :start_time
+  attr_accessible :description, :end_time, :name, :start_time, :facebook_id
   belongs_to :club
   has_many :categories, :through => :club, :source => :categories	
+  def self.get_facebook_group_events(graph_id, user_access_token)
+    MiniFB.get(user_access_token, graph_id , :type => "events")
+  end
 end

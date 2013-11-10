150b19eeb08180e82c483afc656937c7017d9444
David Su <dtscraft@gmail.com>
2013-11-09 19:38:40 -0800

    Club#get_facebook_group_events
    made get_facebook_group_events an instance method of club instead of
    class method of Event

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,4 +2,8 @@ class Club < ActiveRecord::Base
   attr_accessible :description, :name, :facebook_id
   has_many :events 
   has_many :categories
+  def get_facebook_group_events(user_access_token)
+    MiniFB.get(user_access_token, facebook_id , :type => "events") if facebook_id
+  end
+
 end
--- features/step_definitions/group_events.rb
+++ features/step_definitions/group_events.rb
@@ -4,9 +4,9 @@ When /I get facebook events for Facebook Group "([^"]*)" with user access token 
       url = "https://graph.facebook.com/#{graph_id}/events?access_token=#{user_access_token}"
       body = File.open( File.join(File.expand_path(File.dirname(__FILE__)), "..", "support", "events.json"), "r").read
       FakeWeb.register_uri(:get, url, :body => body)
-      events = Event.get_facebook_group_events(graph_id, user_access_token)
+      club = (Club.find_by_facebook_id(graph_id) or Club.create!(facebook_id: graph_id))
+      events = club.get_facebook_group_events(user_access_token)
       events.data.each{ |event|
-          club = (Club.find_by_facebook_id(graph_id) or Club.create!(facebook_id: graph_id))
           new_event = Event.create!(name: event.name, start_time: event.start_time, end_time: event.end_time, facebook_id: event.id)
           club.events << new_event and club.save!
       }
